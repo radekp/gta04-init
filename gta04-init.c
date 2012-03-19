4ad3f33cbb973d358da5baa48bfbb8b4d36752cf
Radek Polak <psonek2@seznam.cz>
2012-03-19 23:00:52 +0100

    Draw nice bmp icons on framebuffer

--- gta04-init.c
+++ gta04-init.c
@@ -21,8 +21,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/mount.h>
+#include <linux/fb.h>
 #include <linux/input.h>
 
 // Write string count bytes long to file
@@ -41,45 +43,83 @@ void write_file(const char *path, const char *value)
     writen_file(path, value, strlen(value));
 }
 
-const char *menu =
-    "                   GTA04 boot\n"
-    "\n"
-    "+===========================+==============================+\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|        SSSS   DDDD        |  N   N   AAA   N   N  DDDD   |\n"
-    "|       S       D   D       |  NN  N  A   A  NN  N  D   D  |\n"
-    "|        SSSS   D   D       |  N N N  AAAAA  N N N  D   D  |\n"
-    "|            S  D   D       |  N  NN  A   A  N  NN  D   D  |\n"
-    "|        SSSS   DDDD        |  N   N  A   A  N   N  DDDD   |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "+===========================+==============================+\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|             1             |            2222              |\n"
-    "|           1 1             |           2    2             |\n"
-    "|         1   1             |               2              |\n"
-    "|             1             |             2                |\n"
-    "|             1             |           222222             |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "|                           |                              |\n"
-    "+===========================+==============================+\n";
+#define BMP_WIDTH 128
+#define BMP_HEIGHT 128
+#define BMP_PIX_BYTES 4
+#define BMP_LINE_LEN (BMP_WIDTH * BMP_PIX_BYTES)
+#define BMP_SIZE (BMP_WIDTH * BMP_HEIGHT * BMP_PIX_BYTES)
+
+int fb_draw(const char *path, int left, int top)
+{
+    int filefd = -1;
+    int fbfd = -1;
+    char *filemap = NULL;
+    char *fbmap = NULL;
+    size_t fblen;
+    struct fb_var_screeninfo screen_info;
+    struct fb_fix_screeninfo fixed_info;
+    struct stat st;
+    int y;
+
+    filefd = open(path, O_RDONLY);
+    if (filefd == -1) {
+        perror("open failed");
+        goto cleanup;
+    }
+
+    if (fstat(filefd, &st) < 0) {
+        perror("fstat failed");
+        goto cleanup;
+    }
+
+    filemap = mmap(0, st.st_size, PROT_READ, MAP_SHARED, filefd, 0);
+    if (filemap == MAP_FAILED) {
+        close(filefd);
+        perror("file mmap failed");
+        goto cleanup;
+    }
+
+    fbfd = open("/dev/fb0", O_RDWR);
+    if (fbfd < 0) {
+        perror("fb open failed");
+        goto cleanup;
+    }
+
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &screen_info) ||
+        ioctl(fbfd, FBIOGET_FSCREENINFO, &fixed_info)) {
+        perror("fb info failed");
+        goto cleanup;
+    }
+
+    fblen = screen_info.yres_virtual * fixed_info.line_length;
+    fbmap = mmap(NULL, fblen, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+
+    if (fbmap == MAP_FAILED) {
+        perror("fb mmap failed");
+        goto cleanup;
+    }
+    for (y = 0; y < BMP_HEIGHT; y++) {
+        memmove(fbmap + left * BMP_PIX_BYTES +
+                (top + y) * fixed_info.line_length,
+                filemap + st.st_size - (y + 1) * BMP_LINE_LEN, BMP_LINE_LEN);
+    }
+
+cleanup:
+
+    if (fbmap && fbmap != MAP_FAILED)
+        munmap(fbmap, fblen);
+
+    if (fbfd >= 0)
+        close(fbfd);
+
+    if (filemap && filemap != MAP_FAILED)
+        munmap(filemap, st.st_size);
+
+    if (filefd >= 0)
+        close(filefd);
+
+    return 0;
+}
 
 int main()
 {
@@ -90,7 +130,10 @@ int main()
     const char *choice = NULL;
 
     printf("gta04-init\n");
-    write_file("/dev/tty0", menu);
+    fb_draw("/pic/sd.bmp", 56, 96);
+    fb_draw("/pic/nand.bmp", 56 + 240, 96);
+    fb_draw("/pic/1.bmp", 56, 320 + 96);
+    fb_draw("/pic/2.bmp", 56 + 240, 320 + 96);
 
     // Mount the FAT boot partition
     for (i = 0;; i++) {
